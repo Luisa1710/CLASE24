d01b54585aab5b9d2923d9d07d0de924cf6ed58c
Juan Manuel Londoño <juanchiixx1998@gmail.com>
2022-06-24 12:33:53 -0500

    Se crea interfaz index

---
+++ src/controllers/controladorindex.js
@@ -0,0 +1,21 @@
+//ENTRADAS = VARIABLES
+let nombre = "Juan"
+let numero = 5
+let politico = {
+    nombre: "Alvaro",
+    poder: "dinero",
+    edad : 64
+}
+
+
+
+// CONTROLAR ETIQUETA 
+// 1. NECESITO aLMACENAR UNA VARIABLE
+let titulo = document.getElementById ("titulo")
+let imagen = document.getElementById ("foto")
+// 2. IDENTIFICO QUE QUIERO CONTROLAR
+// 2.1 CAMBIANDO EL TEXTO
+
+titulo.textContent = "Chao"
+// 2.2 CAMBIANDO LA FUENTE
+imagen.src = "https://placeimg.com/640/480/animals"
